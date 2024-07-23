83f409c26385493c7f7d22f8964c07252e1d88f7
Gabie <67624149+gabizzle@users.noreply.github.com>
2024-07-23 08:56:23 -0400

    New file

---
+++ Loops/asterisks.py
@@ -0,0 +1,17 @@
+# Asterisks - Loops
+
+'''
+    Create a program that uses a for loop and range() function to display the following pattern in the terminal using a bunch of asterisks *:
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    # ... and so on
+
+    It should look like this but with 25 rows total.
+'''
+
+for i in range(25):
+  print('* ' * i)
